64979152249b76dc369d27883951d842dd9d5dbe
Nour Rayan <n6rayan@gmail.com>
2022-02-12 19:04:41 +0000

    Revert "fix: missing headers"
    This reverts commit e186d4076c5a905c4f5b8b6145e2db192c95d719.

--- src/services/base.ts
+++ src/services/base.ts
@@ -2,13 +2,6 @@ import db from '../../db/models';
 
 const { sequelize } = db;
 
-const headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Credentials': true,
-  'Access-Control-Allow-Headers': '*',
-  'Content-Type': 'application/json',
-};
-
 export default async (fn) => {
   console.log('[Base] Executing...');
 
