83afccc30a1b997e05a720be10131c9e630583ec
Nour Rayan <n6rayan@gmail.com>
2022-02-25 21:54:51 +0000

    feat(VLT-13): get accounts, balances and transactions (#9)

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ export default {
   },
   trueLayer: {
     apiUrl: process.env.TRUE_LAYER_API_URL,
+    authUrl: process.env.TRUE_LAYER_AUTH_URL,
     clientId: process.env.TRUE_LAYER_CLIENT_ID,
     clientSecret: process.env.TRUE_LAYER_CLIENT_SECRET,
     redirectUrl: process.env.TRUE_LAYER_REDIRECT_URL,
